0d80ebfd4ed95d3d9252e5b64802aa6ecbbb1a65
Annaofdoom <32620147+Annaofdoom@users.noreply.github.com>
2021-01-31 19:20:29 +0100

    Saved my old project chatbot project last time by mistake. This is the real deal

--- code/script.js
+++ code/script.js
@@ -1,121 +1,407 @@
 // All the DOM selectors stored as short variables
-const chat = document.getElementById("chat")
-const nameForm = document.getElementById("name-form")
-const inputwrapper = document.getElementById("input-wrapper")
+const board = document.getElementById('board')
+const questions = document.getElementById('questions')
+const restartButton = document.getElementById('restart')
+const filterButton = document.getElementById('filter')
+const winOrLoseText = document.getElementById('winOrLoseText')
+const winOrLose = document.getElementById('winOrLose')
+const playAgainButton = document.getElementById('playAgain')
 
-// Global variables, if you need any, declared here
+// Array with all the characters, as objects
+const CHARACTERS = [
+  {
+    name: 'Jabala',
+    img: 'images/jabala.svg',
+    hairColor: 'hidden',
+    eyeColor: 'hidden',
+    glasses: true,
+    hat: true,
+    smoker: false,
+  },
+  {
+    name: 'Jack',
+    img: 'images/jack.svg',
+    hairColor: 'hidden',
+    eyeColor: 'blue',
+    glasses: false,
+    hat: true,
+    smoker: false,
+  },
+  {
+    name: 'Jacques',
+    img: 'images/jacques.svg',
+    hairColor: 'grey',
+    eyeColor: 'blue',
+    glasses: false,
+    hat: true,
+    smoker: true,
+  },
+  {
+    name: 'Jai',
+    img: 'images/jai.svg',
+    hairColor: 'black',
+    eyeColor: 'brown',
+    glasses: false,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jake',
+    img: 'images/jake.svg',
+    hairColor: 'yellow',
+    eyeColor: 'green',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'James',
+    img: 'images/james.svg',
+    hairColor: 'brown',
+    eyeColor: 'green',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jana',
+    img: 'images/jana.svg',
+    hairColor: 'black',
+    eyeColor: 'hidden',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jane',
+    img: 'images/jane.svg',
+    hairColor: 'yellow',
+    eyeColor: 'hidden',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jaqueline',
+    img: 'images/jaqueline.svg',
+    hairColor: 'orange',
+    eyeColor: 'green',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
 
-// Functions declared here
+  {
+    name: 'Jazebelle',
+    img: 'images/jazebelle.svg',
+    hairColor: 'purple',
+    eyeColor: 'hidden',
+    glasses: true,
+    hat: false,
+    smoker: true,
+  },
+  {
+    name: 'Jean',
+    img: 'images/jean.svg',
+    hairColor: 'brown',
+    eyeColor: 'blue',
+    glasses: true,
+    hat: true,
+    smoker: true,
+  },
+  {
+    name: 'Jeane',
+    img: 'images/jeane.svg',
+    hairColor: 'brown',
+    eyeColor: 'green',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jed',
+    img: 'images/jed.svg',
+    hairColor: 'orange',
+    eyeColor: 'green',
+    glasses: true,
+    hat: true,
+    smoker: true,
+  },
+  {
+    name: 'Jenni',
+    img: 'images/jenni.svg',
+    hairColor: 'white',
+    eyeColor: 'hidden',
+    glasses: false,
+    hat: true,
+    smoker: false,
+  },
+  {
+    name: 'Jeri',
+    img: 'images/jeri.svg',
+    hairColor: 'orange',
+    eyeColor: 'green',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jerry',
+    img: 'images/jerry.svg',
+    hairColor: 'hidden',
+    eyeColor: 'blue',
+    glasses: false,
+    hat: true,
+    smoker: false,
+  },
+  {
+    name: 'Jess',
+    img: 'images/jess.svg',
+    hairColor: 'black',
+    eyeColor: 'blue',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jocelyn',
+    img: 'images/jocelyn.svg',
+    hairColor: 'black',
+    eyeColor: 'brown',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jon',
+    img: 'images/jon.svg',
+    hairColor: 'brown',
+    eyeColor: 'green',
+    glasses: true,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jordan',
+    img: 'images/jordan.svg',
+    hairColor: 'yellow',
+    eyeColor: 'hidden',
+    glasses: true,
+    hat: true,
+    smoker: false,
+  },
+  {
+    name: 'Josephine',
+    img: 'images/josephine.svg',
+    hairColor: 'grey',
+    eyeColor: 'brown',
+    glasses: false,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Josh',
+    img: 'images/josh.svg',
+    hairColor: 'yellow',
+    eyeColor: 'green',
+    glasses: false,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Jude',
+    img: 'images/jude.svg',
+    hairColor: 'black',
+    eyeColor: 'green',
+    glasses: false,
+    hat: false,
+    smoker: false,
+  },
+  {
+    name: 'Julie',
+    img: 'images/julie.svg',
+    hairColor: 'black',
+    eyeColor: 'brown',
+    glasses: true,
+    hat: true,
+    smoker: false,
+  },
+]
 
-const nameInput = document.getElementById("name-input")
+// Global variables
+let secret, currentQuestion, charactersInPlay
 
-const handleNameInput = (event) => {
-  event.preventDefault()
+// Draw the game board
+const generateBoard = () => {
+  board.innerHTML = ''
+  charactersInPlay.forEach((person) => {
+    board.innerHTML += `
+      <div class="card">
+        <p>${person.name}</p>
+        <img src=${person.img} alt=${person.name}>
+        <div class="guess">
+          <span>Guess on ${person.name}?</span>
+          <button class="filled-button small" onclick="guess('${person.name}')">Guess</button>
+        </div>
+      </div>
+    `
+  })
+}
 
-  showMessage(nameInput.value, "user")
+// Randomly select a person from the characters array and set as the value of the variable called secret
+const setSecret = () => {
+  secret = charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)]
+}
 
-  nameInput.value = ""
+// This function to start (and restart) the game
+const start = () => {
+  // Here we're setting charactersInPlay array to be all the characters to start with
+  charactersInPlay = CHARACTERS
 
-  setTimeout(()=> question1(), 1000)
+  board.style.display = "flex"
+  winOrLose.style.display = "none"
+  // What else should happen when we start the game?
+  setSecret()
+  generateBoard()
+}
 
+// setting the currentQuestion object when you select something in the dropdown
+const selectQuestion = () => {
+  const category = questions.options[questions.selectedIndex].parentNode.label
+  // This variable stores what option group (category) the question belongs to.
+  // We also need a variable that stores the actual value of the question we've selected.
+  const value = questions.value
   
-
-  }
-
-  const question1 = () => {
-    showMessage("Hur ofta vill du spara?", "bot")
-    inputwrapper.innerHTML = `
-    <button id="button1">Lite varje månad</button>
-    <button id="button2">Ett engångsbelopp</button>`
-    document.getElementById("button1")
-    .addEventListener("click", () => question2())
-    document.getElementById("button2")
-    .addEventListener("click", () => answer1())
-
-  }
-
-  const question2 = () => {
-    showMessage("Strålande! Att spara lite varje månad gör att du sprider riskerna. Hur mycket vill du spara i månaden?", "bot")
-    inputwrapper.innerHTML = `
-    <button id="button3">Över 500 kr</button>
-    <button id="button4">Under 500 kr</button>`
-    document.getElementById("button3")
-    .addEventListener("click", () => question3())
-    document.getElementById("button4")
-    .addEventListener("click", () => answer1())
-  }
-
-  const question3 = () => {
-    showMessage("Så den sista och kanske viktigaste frågan! Vad brinner du mest för?", "bot")
-    inputwrapper.innerHTML = `
-    <button id="button5">Miljö & klimat</button>
-    <button id="button6">Sociala frågor</button>
-    <button id="button7">Jämställdhet</button>`
-    document.getElementById("button5")
-    .addEventListener("click", () => answer2())
-    document.getElementById("button6")
-    .addEventListener("click", () => answer2())
-    document.getElementById("button7")
-    .addEventListener("click", () => answer3())
-  }
-
-  const answer1 = () => {
-    showMessage("Hållbara aktier kanske passar dig!", "bot")
-    inputwrapper.innerHTML = `
-    <button onclick="window.location.href=('https://www.aktieinvest.se/hallbart-sparande/hallbara-aktier')">Läs mer om hållbara aktier!</button>`
-  }
-
-  const answer2 = () => {
-    showMessage("Hållbarhetsboxen kanske passar dig!", "bot")
-    inputwrapper.innerHTML = `
-    <button onclick="window.location.href=('https://www.aktieinvest.se/hallbarhetsboxen')">Läs mer om hållbarhetsboxen!</button>`
-  }
-
-  const answer3 = () => {
-    showMessage("Framtidsfeministens sparbox kanske passar dig!", "bot")
-    inputwrapper.innerHTML = `
-    <button onclick="window.location.href=('https://www.aktieinvest.se/framtidsfeministen')">Läs mer om Framtidsfeministens sparbox!</button>`
-  }
-
-// This function will add a chat bubble in the correct place based on who the sender is
-function showMessage(message, sender) {
-  if (sender === "user") {
-    chat.innerHTML += `
-      <section class="user-msg">
-        <div class="bubble user-bubble">
-          <p>${message}</p>
-        </div>
-        <img src="assets/user.png" alt="User" />  
-      </section>
-    `
-  } else if (sender === "bot") {
-    chat.innerHTML += `
-      <section class="bot-msg">
-        <img src="assets/aiv-bot_400x400.jpg" alt="Bot" />
-        <div class="bubble bot-bubble">
-          <p>${message}</p>
-        </div>
-      </section>
-    `
-  }
-  // This little thing makes the chat scroll to the last message when there are too many to be shown in the chat box
-  chat.scrollTop = chat.scrollHeight
+  if (category === 'hair color') {
+    currentQuestion = {
+      attribute: 'hairColor',
+      value: value,
+      category: category,
+    }
+  } else if (category === 'eye color') {
+    currentQuestion = {
+      attribute: 'eyeColor',
+      value: value,
+      category: category,
+    }
+  } else if (category === 'accessories') {
+    currentQuestion = {
+      attribute: value,
+      value: true,
+      category: category,
+    } 
+    
+  } else if (category === 'other') {
+    currentQuestion = {
+      attribute: 'smoker',
+      value: true,
+      category: category,
+    }
+  } 
 }
 
-// Starts here
-const greeting = () => {
-  showMessage(`Hej på dig! Vad heter du?`, "bot")
-  // Just to check it out, change "bot" to "user" here 👆
+// This function should be invoked when you click on 'Find Out'.
+const checkQuestion = () => {
+const keep = currentQuestion.value === setSecret(currentQuestion.attribute)
+filterCharacters(keep)
+  // Compare the currentQuestion with the secret person.
+  // See if we should keep or remove people based on that
+  // Then invoke filterCharacters
 }
 
-// Set up your eventlisteners here
+// It'll filter the characters array and redraw the game board.
+const filterCharacters = (keep) => {
+  // Show the correct alert message for different categories
+  const { attribute, category, value } = currentQuestion
+  if (category === 'accessories') {
+    if (keep) {
+      alert(
+        `Yes, the person wears ${attribute}! Keep all that wears ${attribute}`
+      )
+    } else {
+      alert(
+        `No, the person doesn't wear ${attribute}! Remove all that wears ${attribute}`
+      )
+    }
+  } else if (category === 'eye color') {
+     if (keep) {
+      alert(
+        `Yes, the person have ${value} eyes! Keep all that have ${value} eyes!`
+      )
+    } else {
+      alert(
+        `No, the person do not have ${value} eyes! Remove all that have ${value} eyes!`
+      )
+    }
+  }
+  
+  else if (category === 'other') {
+    if (keep) {
+      alert(
+        `Yes, the person smokes! Keep all persons that smoke`
+      )
+    } else {
+      alert(
+        `No, the person doesn't smoke! Remove all persons that smoke`
+      )
+    }
+    // Similar to the one above
+  } 
+  else if (category === 'hair color') {
+    if (keep) {
+      alert(`Yes, the person has ${value} hair! Keep all persons with ${value} hair`)
+      // alert popup that says something like: "Yes, the person has yellow hair! Keep all persons with yellow hair"
+    } else {
+      alert(`No, the person doesn't have ${value} hair. Remove all persons with ${value} hair`)
+      // alert popup that says something like: "NO, the person doesnt have yellow hair! Remove all persons with yellow hair"
+    } 
+  }
 
-document.getElementById("name-form").addEventListener("submit", handleNameInput)
+  // filter to keep or remove based on the keep variable.
+  if (keep) {
+    charactersInPlay = charactersInPlay.filter((person) => person[attribute] === value)
+  } else {
+    charactersInPlay = charactersInPlay.filter((person) => person[attribute] !== value)
+  }
+ generateBoard()
 
-// When website loaded, chatbot asks first question.
-// normally we would invoke a function like this:
-// greeting()
-// But if we want to add a little delay to it, we can wrap it in a setTimeout:
-// setTimeout(functionName, timeToWaitInMilliSeconds)
-// This means the greeting function will be called one second after the website is loaded.
-setTimeout(greeting, 1000)
+  // Invoke a function to redraw the board with the remaining people.
+}
+
+// when clicking guess, the player first have to confirm that they want to make a guess.
+const guess = (suspect) => {
+  const isConfirmed = confirm(`Are you sure you want to make a guess on ${suspect}?`)
+  if (isConfirmed) {
+    checkMyGuess(suspect)
+  }
+  // store the interaction from the player in a variable.
+  // remember the confirm() ?
+  // If the player wants to guess, invoke the checkMyGuess function.
+}
+
+// If you confirm, this function is invoked
+const checkMyGuess = (suspect) => {
+  if (suspect === secret.name) {
+    winOrLoseText.innerHTML = `You guessed on ${suspect} and it was correct!`
+  } else {
+    winOrLoseText.innerHTML = `Wrong answer. It was actually ${secret.name}!`
+  }
+  console.log("suspect", suspect)
+  console.log("secret name", secret.name)
+  // 1. Check if the suspect is the same as the secret person's name
+  // 2. Set a Message to show in the win or lose section accordingly
+  // 3. Show the win or lose section
+  // 4. Hide the game board
+  winOrLose.style.display = "flex"
+board.style.display = "none"
+}
+
+
+// Invokes the start function when website is loaded
+start()
+
+// All the event listeners
+playAgainButton.addEventListener('click', start)
+restartButton.addEventListener('click', start)
+questions.addEventListener('change', () => selectQuestion())
+filterButton.addEventListener('click', checkQuestion)
